40bdb08a8f7927e7eb289190ea4a7c638b32cae2
RedBoxing <thomas200574@gmail.com>
2021-08-09 20:10:45 +0200

    fix again the models

--- src/database/schema/CosmeticsPack.ts
+++ src/database/schema/CosmeticsPack.ts
@@ -14,6 +14,6 @@ export class CosmeticsPack extends Model {
     @BelongsTo(() => User, 'publisher_id')
     publisher!: User;
 
-    @HasMany(() => Cosmetic)
+    @HasMany(() => Cosmetic, 'id')
     cosmetics!: Cosmetic[];
 }
